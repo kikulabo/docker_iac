9f205a8a8708174716623c30bbff1ef8d0d10a8c
kikulabo <n.kikuchi.7ahl@gmail.com>
2021-11-06 20:02:37 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "docker_iac_vpc"
+  }
+}
