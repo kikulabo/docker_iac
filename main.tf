2d1b6c6647924c896d5c9ca583990aa9da96986b
kikulabo <n.kikuchi.7ahl@gmail.com>
2021-11-06 20:36:47 +0900

    backendを追加

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-236614022713"
+    key    = "docker_iac.terraform.tfstate.aws"
+    region = "ap-northeast-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.10.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-1"
 }
